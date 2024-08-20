f2ad68d2704aa359e0205a9896f50169029125b2
Pablo Conejos <126771457+pabloconejos@users.noreply.github.com>
2024-08-20 12:39:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-export const {
-  PORT = 3000,
-  SALT_ROUNDS = 10,
-  SECRET_JWT_KEY = 'this-is-a-secret-key-,/L6p?F{icu35s8<j7e4,',
-  AUTHTOKEN = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjI3ODk3ODUsImlkIjoiZjI2ZTIwOWUtMmRiMC00NjQwLWEzMGUtZTUzYjE4MTJiMWFmIn0.taEPXBUhy72bgd5lJjy_M4ZjjK1Z0CdUuXASmoPDMl5TTkcR6KIyISgHGrclwuUuevnqhL64-OR1uPdQ_YfpBA',
-  URI = 'libsql://my-links-db-pabloconejos.turso.io'
-} = process.env
+
